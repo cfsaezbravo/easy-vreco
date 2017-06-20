2b0c3eb75f5ef5ff8fc8b1464cc0fe86264f4f12
Camila Sáez <cfsaezbravo@gmail.com>
2017-06-20 09:53:59 -0400

    Did it

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,5 +32,35 @@ function initMap() {
 	var funcionError = function (error) {
 		alert("Tenemos un problema con encontrar tu ubicación");
 	}
+
+	var start = document.getElementById('origen');
+	var end = document.getElementById('destino');
+
+	new google.maps.places.Autocomplete(start);
+	new google.maps.places.Autocomplete(end);
+
+	var directionsService = new google.maps.DirectionsService;
+	var directionsDisplay = new google.maps.DirectionsRenderer;
+
+	var calcularRuta = function(directionsService,directionsDisplay){
+		directionsService.route({
+		origin: start.value,
+		destination: end.value,
+		travelMode: 'DRIVING'
+		}, function (response,status){
+			if (status === 'OK') {
+	      	directionsDisplay.setDirections(response);
+	    	}else{
+			window.alert("No encontramos una ruta");
+			}
+		})
+	};
+
+		directionsDisplay.setMap(map);
+		var trazarRuta = function(){
+			calcularRuta(directionsService,directionsDisplay);
+		};
+
+		document.getElementById("ruta").addEventListener('click', trazarRuta);  
 };
 
